67220babfed16f4a6ae551009e8d20e1464a91ad
Paul Bouchequet <paul.bouchequet@gmail.com>
2018-09-16 23:20:18 +0200

    download funcs

---
+++ R/download.R
@@ -0,0 +1,20 @@
+# #http://sleeptight.isr.uc.pt/ISRUC_Sleep/
+# download_isruc <- function(path){
+#   #http://sleeptight.isr.uc.pt/ISRUC_Sleep/ISRUC_Sleep/subgroupI/1.rar
+# }
+# 
+# #https://www.physionet.org/pn4/sleep-edfx/
+# #https://physionet.org/physiobank/database/sleep-edfx/
+# download_sleepedfx <- function(path){
+#   
+# }
+# 
+# #http://www.tcts.fpms.ac.be/~devuyst/Databases/DatabaseSubjects/
+# download_dreams <- function(path){
+#   
+# }
+# 
+# #https://physionet.org/physiobank/database/capslpdb/
+# download_capsleep <- function(path){
+#   
+# }
