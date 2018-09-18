20b2022a48acde5b4b173b42efa4c8dd984be959
Paul Bouchequet <paul.bouchequet@gmail.com>
2018-09-18 12:16:12 +0200

    download dreams spindle

--- R/download.R
+++ R/download.R
@@ -18,3 +18,23 @@
 # download_capsleep <- function(path){
 #   
 # }
+
+# http://www.tcts.fpms.ac.be/~devuyst/Databases/DatabaseSpindles/
+download_dreams_spindles <- function(path){
+  if(!dir.exists(path)){
+    dir.create(path)
+  }
+  rar_md5 <- "2f8a101194e133dd4324f21047dce579"
+  rar_path <- paste0(path,"/DatabaseSpindles.rar")
+  if(!file.exists(rar_path) & !(rar_md5 == tools::md5sum(rar_path))){
+    download.file(url = "http://www.tcts.fpms.ac.be/~devuyst/Databases/DatabaseSpindles/DatabaseSpindles.rar",
+                  destfile = rar_path)
+  }
+  if(!file.exists(paste0(path,"excerpt2.edf"))){
+    if(rar_md5 == tools::md5sum(rar_path)){
+      system(paste0("unrar x ",rar_path," ",path))
+    } else {
+      stop("Invalid MD5")
+    }
+  }
+}
